6edc0a13ec8c2339817f2f7e00f3774767dc6469
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2021-01-07 15:44:03 +0600

    Converting into PWA

--- sw.js
+++ sw.js
@@ -31,5 +31,9 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-	console.log("Fetch intercepted for:", event.request.url);
+	event.respondWith(
+		caches.match(event.request).then((cachedResponse) => {
+			return cachedResponse || fetch(event.request);
+		})
+	);
 });
